12b9b68e4a44a43973e89a614829dedfb00e0742
trentungard <trentungard@gmail.com>
2022-08-06 23:49:36 -0400

    adding tokens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,7 @@
+const colors = require('./tokens/colors');
+const fontSize = require('./tokens/fontSize');
+const spacing  = require('./tokens/spacing');
+
 module.exports = {
   content: [
     "./pages/**/*.{html,js,jsx,ts,tsx}",
@@ -6,32 +10,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "accent-1": "#FAFAFA",
-        "accent-2": "#EAEAEA",
-        "accent-7": "#333",
-        success: "#0070f3",
-        cyan: "#79FFE1",
-        "ungard-blue": "#4B50C4",
-        "ungard-dark": "#292B4D"
+        colors
       },
       spacing: {
-        28: "7rem",
-      },
-      letterSpacing: {
-        tighter: "-.04em",
-      },
-      lineHeight: {
-        tight: 1.2,
+        spacing
       },
       fontSize: {
-        "5xl": "2.5rem",
-        "6xl": "2.75rem",
-        "7xl": "4.5rem",
-        "8xl": "6.25rem",
-      },
-      boxShadow: {
-        small: "0 5px 10px rgba(0, 0, 0, 0.12)",
-        medium: "0 8px 30px rgba(0, 0, 0, 0.12)",
+        fontSize
       },
     },
   },
---
+++ tokens/colors.js
@@ -0,0 +1,11 @@
+exports.colors = {
+    "accent-1": "#FAFAFA",
+    "accent-2": "#EAEAEA",
+    "accent-7": "#333",
+    success: "#0070f3",
+    cyan: "#79FFE1",
+    "avenue-blue": "#5B60C9",
+    "ungard-blue": "#4347A7",
+    moonbeam: "#292B4D",
+    "high yellow": "#F9F871"
+}
