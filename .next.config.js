77bec33e283af62d254a7d0e02947dd65b1637dd
trentungard <trentungard@gmail.com>
2022-08-25 20:57:27 -0400

    next public

--- .next.config.js
+++ .next.config.js
@@ -1,11 +1,14 @@
 module.exports = {
-    async redirects() {
-      return [
-        {
-          source: '/*',
-          destination: '/',
-          permanent: true,
-        },
-      ]
-    },
-  }
+  env: {
+    IS_MAINTENANCE_MODE: process.env.IS_MAINTENANCE_MODE,
+  }, 
+  async redirects() {
+    return [
+      {
+        source: '/*',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
+}
